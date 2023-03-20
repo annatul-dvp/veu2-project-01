41e7d1a4de8e5560a58f793c8499668afae5cfe1
Anna Tuluptseva <anya.tul@yandex.ru>
2023-03-20 21:43:07 +0300

    Task 5.1 is done

---
+++ 04_pagination_and_filtering/vue2/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <component :is="currentPageComponent" :page-params="currentPageParams"
+  @gotoPage="(pageName, pageParams) => gotoPage(pageName, pageParams)"/>
+</template>
+
+<script>
+
+import MainPage from '@/pages/MainPage.vue';
+import ProductPage from '@/pages/ProductPage.vue';
+import NotFoundPage from '@/pages/NotFoundPage.vue';
+
+const routes = {
+  main: 'MainPage',
+  product: 'ProductPage',
+};
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      currentPage: 'main',
+      currentPageParams: {},
+    };
+  },
+  methods: {
+    gotoPage(pageName, pageParams) {
+      this.currentPage = pageName;
+      this.currentPageParams = pageParams || {};
+    },
+  },
+  computed: {
+    currentPageComponent() {
+      return routes[this.currentPage] || 'NotFoundPage';
+    },
+  },
+  components: { MainPage, ProductPage, NotFoundPage },
+};
+</script>
---
+++ 04_pagination_and_filtering/vue2/src/components/ProductsList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem v-for="(product) in products" :key="product.id" :product ='product'
+    @gotoPage="(pageName, pageParams) => $emit('gotoPage', pageName, pageParams)">
+    </ProductItem>
+  </ul>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue';
+
+export default {
+  props: ['products'],
+  components: { ProductItem },
+};
+</script>
