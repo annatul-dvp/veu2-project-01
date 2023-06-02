cc2d7d18f2b7b02366ef6fcba89b13409e71e28c
Anna Tuluptseva <anya.tul@yandex.ru>
2023-06-02 21:52:13 +0300

    Vue2. 7.9. Corretion

--- 06_basket/vue2/src/pages/ProductPage.vue
+++ 06_basket/vue2/src/pages/ProductPage.vue
@@ -44,25 +44,12 @@
             <fieldset class="form__block">
               <legend class="form__legend">Цвет:</legend>
               <ul class="colors">
-                <li class="colors__item">
+                <li class="colors__item" v-for="(c) in product.colors" :key="c.id">
                   <label class="colors__label">
-                    <input class="colors__radio sr-only" type="radio" name="color-item" value="blue" checked="">
-                    <span class="colors__value" style="background-color: #73B6EA;">
+                    <input class="colors__radio sr-only" type="radio" name="color-item" :value="c.code" v-model="color">
+                    <span class="colors__value" :style="{backgroundColor: c.code}">
                     </span>
                   </label>
-                </li>
-                <li class="colors__item">
-                  <label class="colors__label">
-                    <input class="colors__radio sr-only" type="radio" name="color-item" value="yellow">
-                    <span class="colors__value" style="background-color: #FFBE15;">
-                    </span>
-                  </label>
-                </li>
-                <li class="colors__item">
-                  <label class="colors__label">
-                    <input class="colors__radio sr-only" type="radio" name="color-item" value="gray">
-                    <span class="colors__value" style="background-color: #939393;">
-                  </span></label>
                 </li>
               </ul>
             </fieldset>
@@ -191,6 +178,7 @@ export default {
     return {
       pageType: 'productPage',
       productAmount: 1,
+      color: '#73B6EA',
 
       productData: null,
 
