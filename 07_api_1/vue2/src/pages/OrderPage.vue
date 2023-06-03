242a5092341c9191f4ce6cf198b1eeebce8b0146
Anna Tuluptseva <anya.tul@yandex.ru>
2023-06-03 23:51:20 +0300

    Vue2. 8.1. Task 1

---
+++ 07_api_1/vue2/src/pages/OrderPage.vue
@@ -0,0 +1,154 @@
+<!-- eslint-disable vuejs-accessibility/form-control-has-label -->
+<template>
+  <main class="content container">
+    <div class="content__top">
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="index.html">
+            Каталог
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="cart.html">
+            Корзина
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link">
+            Оформление заказа
+          </a>
+        </li>
+      </ul>
+
+      <h1 class="content__title">
+        Корзина
+      </h1>
+      <span class="content__info">
+        3 товара
+      </span>
+    </div>
+
+    <section class="cart">
+      <form class="cart__form form" action="#" method="POST">
+        <div class="cart__field">
+          <div class="cart__data">
+            <BaseFormText v-model="formData.name" title="ФИО" :error="formError.name" placeholder="Введите ваше полное имя"/>
+
+            <BaseFormText v-model="formData.address" title="Адрес доставки" :error="formError.address" placeholder="Введите ваш адрес"/>
+
+            <BaseFormText v-model="formData.phone" title="Телефон" type="tel" :error="formError.phone" placeholder="Введите ваш телефон"/>
+
+            <BaseFormText v-model="formData.email" title="Email" type="email" :error="formError.email" placeholder="Введи ваш Email"/>
+
+            <BaseFormTextArea v-model="formData.comments" title="Комментарий к заказу" :error="formError.comments" placeholder="Ваши пожелания"/>
+          </div>
+
+          <div class="cart__options">
+            <h3 class="cart__title">Доставка</h3>
+            <ul class="cart__options options">
+              <li class="options__item">
+                <label class="options__label">
+                  <input class="options__radio sr-only" type="radio" name="delivery" value="0" checked="">
+                  <span class="options__value">
+                    Самовывоз <b>бесплатно</b>
+                  </span>
+                </label>
+              </li>
+              <li class="options__item">
+                <label class="options__label">
+                  <input class="options__radio sr-only" type="radio" name="delivery" value="500">
+                  <span class="options__value">
+                    Курьером <b>500 ₽</b>
+                  </span>
+                </label>
+              </li>
+            </ul>
+
+            <h3 class="cart__title">Оплата</h3>
+            <ul class="cart__options options">
+              <li class="options__item">
+                <label class="options__label">
+                  <input class="options__radio sr-only" type="radio" name="pay" value="card">
+                  <span class="options__value">
+                    Картой при получении
+                  </span>
+                </label>
+              </li>
+              <li class="options__item">
+                <label class="options__label">
+                  <input class="options__radio sr-only" type="radio" name="pay" value="cash">
+                  <span class="options__value">
+                    Наличными при получении
+                  </span>
+                </label>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="cart__block">
+          <ul class="cart__orders">
+            <OrderItem v-for="item in products" :key="item.productID" :item="item"/>
+            <!-- <li class="cart__order">
+              <h3>Смартфон Xiaomi Redmi Note 7 Pro 6/128GB</h3>
+              <b>18 990 ₽</b>
+              <span>Артикул: 150030</span>
+            </li>
+            <li class="cart__order">
+              <h3>Гироскутер Razor Hovertrax 2.0ii</h3>
+              <b>4 990 ₽</b>
+              <span>Артикул: 150030</span>
+            </li>
+            <li class="cart__order">
+              <h3>Электрический дрифт-карт Razor Lil’ Crazy</h3>
+              <b>8 990 ₽</b>
+              <span>Артикул: 150030</span>
+            </li> -->
+          </ul>
+
+          <div class="cart__total">
+            <p>Доставка: <b>{{ orderDeliveryPrice | numberFormat }}</b></p>
+            <p>Итого: <b>{{ orderAmountProducts }}</b> товара на сумму <b>{{ totalPrice | numberFormat }}</b></p>
+          </div>
+
+          <button class="cart__button button button--primery" type="submit">
+            Оформить заказ
+          </button>
+        </div>
+        <div class="cart__error form__error-block">
+          <h4>Заявка не отправлена!</h4>
+          <p>
+            Похоже произошла ошибка. Попробуйте отправить снова или перезагрузите страницу.
+          </p>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import numberFormat from '@/helpers/numberFormat';
+import BaseFormText from '@/components/BaseFormText.vue';
+import BaseFormTextArea from '@/components/BaseFormTextArea.vue';
+import OrderItem from '@/components/OrderItem.vue';
+
+export default {
+  filters: { numberFormat },
+  components: { BaseFormText, BaseFormTextArea, OrderItem },
+  data() {
+    return {
+      formData: {},
+      formError: {},
+    };
+  },
+  computed: {
+    ...mapGetters({
+      products: 'cartDetailProducts',
+      orderDeliveryPrice: 'orderDeliveryPrice',
+      orderAmountProducts: 'orderAmountProducts',
+      totalPrice: 'orderTotalprice',
+    }),
+  },
+};
+</script>
---
+++ 07_api_1/vue2/src/store/index.js
@@ -0,0 +1,140 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+import { API_BASE_URL } from '../config';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    cartProducts: [],
+    userAccessKey: null,
+    cartProductsData: [],
+    orderDeliveryPrice: 500,
+  },
+  mutations: {
+    updateCartProductAmount(state, { productID, amount }) {
+      const item = state.cartProducts.find((i) => i.productID === productID);
+
+      if (item) {
+        item.amount = amount;
+      }
+    },
+    // deleteCartProduct(state, productID) {
+    //   state.cartProducts = state.cartProducts.filter((item) => item.productID !== productID);
+    // },
+    updateUserAccessKey(state, accessKey) {
+      state.userAccessKey = accessKey;
+    },
+    updateCartProductsData(state, items) {
+      state.cartProductsData = items;
+    },
+    syncCartProducts(state) {
+      state.cartProducts = state.cartProductsData.map((item) => ({
+        productID: item.product.id,
+        amount: item.quantity,
+      }));
+    },
+  },
+  getters: {
+    cartDetailProducts(state) {
+      return state.cartProducts.map((item) => {
+        const { product } = state.cartProductsData.find((p) => p.product.id === item.productID);
+        return {
+          ...item,
+          product: {
+            ...product,
+            image: product.image.file.url,
+          },
+        };
+      });
+    },
+    cartTotalprice(state, getters) {
+      return getters.cartDetailProducts.reduce((acc, item) => (item.product.price * item.amount) + acc, 0);
+    },
+    orderTotalprice(state, getters) {
+      return getters.cartDetailProducts.reduce((acc, item) => (item.product.price * item.amount) + acc, 0) + state.orderDeliveryPrice;
+    },
+    orderDeliveryPrice(state) {
+      return state.orderDeliveryPrice;
+    },
+    orderAmountProducts(state, getters) {
+      return getters.cartDetailProducts.reduce((acc, item) => item.amount + acc, 0);
+    },
+  },
+  actions: {
+    loadCart(context) {
+      return axios
+        .get(`${API_BASE_URL}/api/baskets`, {
+          params: {
+            userAccessKey: context.state.userAccessKey,
+          },
+        })
+        .then((response) => {
+          if (!context.state.userAccessKey) {
+            localStorage.setItem('userAccessKey', response.data.user.accessKey);
+            context.commit('updateUserAccessKey', response.data.user.accessKey);
+          }
+          context.commit('updateCartProductsData', response.data.items);
+          context.commit('syncCartProducts');
+        });
+    },
+    addProductToCart(context, { productID, amount }) {
+      return (new Promise((resolve) => { setTimeout(resolve, 2000); }))
+        .then(() => axios
+          .post(`${API_BASE_URL}/api/baskets/products`, {
+            productId: productID,
+            quantity: amount,
+          }, {
+            params: {
+              userAccessKey: context.state.userAccessKey,
+            },
+          })
+          .then((response) => {
+            context.commit('updateCartProductsData', response.data.items);
+            context.commit('syncCartProducts');
+          }));
+    },
+    updateCartProductAmount(context, { productID, amount }) {
+      context.commit('updateCartProductAmount', { productID, amount });
+
+      if (amount < 1) {
+        return undefined;
+      }
+
+      return axios
+        .put(`${API_BASE_URL}/api/baskets/products`, {
+          productId: productID,
+          quantity: amount,
+        }, {
+          params: {
+            userAccessKey: context.state.userAccessKey,
+          },
+        })
+        .then((response) => {
+          context.commit('updateCartProductsData', response.data.items);
+        })
+        .catch(() => {
+          context.commit('syncCartProducts');
+        });
+    },
+    deleteCartProduct(context, { productID }) {
+      return axios.delete(`${API_BASE_URL}/api/baskets/products`, {
+        data: {
+          productId: productID,
+        },
+        params: {
+          userAccessKey: context.state.userAccessKey,
+        },
+      })
+        // eslint-disable-next-line no-shadow
+        .then((response) => {
+          context.commit('updateCartProductsData', response.data.items);
+          context.commit('syncCartProducts');
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+  },
+});
